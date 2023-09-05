28dbefc017cb1011cfa352b405b45b3e63d0a6ae
Ryan Levick <ryan.levick@fermyon.com>
2023-09-05 14:47:44 +0200

    Use llama prompt hits in ts sentiment example
    Signed-off-by: Ryan Levick <ryan.levick@fermyon.com>

--- sentiment-analysis-ts/src/index.ts
+++ sentiment-analysis-ts/src/index.ts
@@ -20,7 +20,11 @@ interface SentimentAnalysisResponse {
 const decoder = new TextDecoder();
 
 const PROMPT = `\
+<<SYS>>
 You are a bot that generates sentiment analysis responses. Respond with a single positive, negative, or neutral.
+<</SYS>>
+[INST]
+Follow the pattern of the following examples:
 
 Hi, my name is Bob
 neutral
@@ -30,6 +34,7 @@ positive
 
 I am so sad today
 negative
+[/INST]
 
 <SENTENCE>
 `;
