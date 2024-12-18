8d1952471f08536a802b1849bbc8d7d887109a36
Mikkel Mørk Hegnhøj <mikkel@fermyon.com>
2024-12-18 12:47:40 +0100

    Updating sentiment-analysis-python to Spin 3.0 / 0.3.3 SDK
    Signed-off-by: Mikkel Mørk Hegnhøj <mikkel@fermyon.com>

--- sentiment-analysis-py/app.py
+++ sentiment-analysis-py/app.py
@@ -1,7 +1,6 @@
-from spin_sdk import http, llm
+from spin_sdk import http, llm, key_value
 from spin_sdk.http import Request, Response
 import json
-import re
 
 PROMPT="""<<SYS>>
 You are a bot that generates sentiment analysis responses. Respond with a single positive, negative, or neutral.
@@ -17,16 +16,44 @@ Bot: positive
 
 User: I am so sad today
 Bot: negative
-[/INST]
 
+[/INST]
 User: """
 
 class IncomingHandler(http.IncomingHandler):
     def handle_request(self, request: Request) -> Response:
+        # Extracting the sentence from the request_body
         request_body=json.loads(request.body)
         sentence=request_body["sentence"].strip()
-        result = llm.infer("llama2-chat", PROMPT+sentence)
-        response_body=json.dumps({"sentiment": re.sub("\\nBot\: ", "", result.text)})
+        print("Performing sentiment analysis on: " + sentence)
+
+        # Open the default KV store
+        store = key_value.open_default()
+
+        # Check if the sentence is already in the KV store
+        if store.exists(sentence) is False:
+            result=llm.infer("llama2-chat", PROMPT+sentence).text
+            print("Raw result: " + result)
+            sentiment = get_sentiment_from_sentence(result)
+            print("Storing result in the KV store")
+            store.set(sentence, str.encode(sentiment))
+        else:
+            sentiment = store.get(sentence).decode()
+            print("Found a cached result")
+
+        response_body=json.dumps({"sentiment": sentiment})
+
         return Response(200,
                         {"content-type": "application/json"},
                         bytes(response_body, "utf-8"))
+
+def get_sentiment_from_sentence(sentence) -> str:
+    words = sentence.lower().split()
+    sentiments = ["positive", "negative", "neutral"]
+    result = next((word for word in sentiments if word in words), None)
+
+    if result is not None:
+        return result
+    else:
+        print("Inconclusive, returning 'neutral'")
+        return "neutral"
